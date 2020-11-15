809156383348a6d5a9c741cdf2cb78f0adb0ccd0
Xinkai Wang <546296781@qq.com>
2020-11-15 04:38:28 -0700

    Create menu

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JMenuItem;
 
-public class Main {
+public class Main extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -13,8 +22,8 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
+					Main frame = new Main();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -23,19 +32,44 @@ public class Main {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Main() {
-		initialize();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 525, 590);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 48, 500, 500);
+		contentPane.add(panel);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(4, 0, 500, 40);
+		contentPane.add(menuBar);
+		
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
+		fileMenu.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(fileMenu);
+		fileMenu.setPreferredSize(new Dimension(100, 40));
+		
+		JMenuItem openItem = new JMenuItem("Open");
+		fileMenu.add(openItem);
+		
+		JMenuItem saveItem = new JMenuItem("Save");
+		fileMenu.add(saveItem);
+		
+		JMenu projectMenu = new JMenu("Project");
+		projectMenu.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
+		menuBar.add(projectMenu);
+		projectMenu.setPreferredSize(new Dimension(100, 40));
+		
+		JMenu aboutMenu = new JMenu("About");
+		aboutMenu.setFont(new Font("Microsoft YaHei UI", Font.BOLD, 12));
+		menuBar.add(aboutMenu);
+		aboutMenu.setPreferredSize(new Dimension(100, 40));
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
 }
---
+++ src/Reporter.java
@@ -0,0 +1,36 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Reporter implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
